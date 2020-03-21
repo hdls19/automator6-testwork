8bba72faad3e4623a96f05bb8aa42504b629bd14
hdls19 <41329018+hdls19@users.noreply.github.com>
2020-03-21 09:45:13 +0200

    Added simple selenium test

--- src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Main.java
+++ src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Main.java
@@ -1,7 +1,26 @@
 package ua.com.qatestlab.prestashop.automation.automator6;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
 	public static void main(String[] args) {
+		//Selenium web driver initialization
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Simple test
+		String baseURL = "https://google.com";
+		String expectedTitle = "Google";
+		
+		driver.get(baseURL);
+		String actualTitle = driver.getTitle();
+		
+		System.out.println("Expected title: " + expectedTitle);
+		System.out.println("Actual title: " + actualTitle);
+		
+		//Close web driver
+		driver.close();
 	}
 }
