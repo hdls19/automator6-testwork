f3f7831ec9c99a6ab2a6887a2731e97c49a7a190
hdls19 <41329018+hdls19@users.noreply.github.com>
2020-03-21 11:24:20 +0200

    Added log4j

--- src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Main.java
+++ src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Main.java
@@ -2,18 +2,30 @@ package ua.com.qatestlab.prestashop.automation.automator6;
 
 import java.io.IOException;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.RollingFileAppender;
+import org.apache.log4j.WriterAppender;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Main {
 
+	private static final Logger LOGGER = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) throws IOException {
+		initLogger();
+		LOGGER.info("=======================================================");
+		
 		//Selenium web driver initialization
+		LOGGER.info("Selenium web driver initialization");
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		//Title test
+		LOGGER.info("Title test");
 		String mainURL = "http://prestashop-automation.qatestlab.com.ua/ru/";
 		String expectedTitle = "prestashop-automation";
 		
@@ -22,11 +34,29 @@ public class Main {
 		MainPage mainPage = new MainPage(driver);
 		
 		String actualTitle = mainPage.getTitle();
-		System.out.println("Actual title: " + actualTitle);
-		System.out.println("Expected title: " + expectedTitle);
+		LOGGER.info("Actual title: " + actualTitle);
+		LOGGER.info("Expected title: " + expectedTitle);
 		
 		//Close web driver
+		LOGGER.info("Close web driver");
 		driver.close();
 		driver.quit();
 	}
+	
+	private static void initLogger() throws IOException {
+    	PatternLayout pattern = new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%t - %m%n");
+
+    	WriterAppender consoleAppender = new WriterAppender(pattern, System.out);
+
+    	consoleAppender.setThreshold(Level.INFO);
+
+    	RollingFileAppender fileAppender = new RollingFileAppender(pattern, "log.txt", true);
+    	fileAppender.setMaxFileSize("5MB");
+    	fileAppender.setMaxBackupIndex(0);
+
+    	fileAppender.setThreshold(Level.INFO);
+
+   		Logger.getRootLogger().addAppender(consoleAppender);
+   		Logger.getRootLogger().addAppender(fileAppender);
+   	}
 }
