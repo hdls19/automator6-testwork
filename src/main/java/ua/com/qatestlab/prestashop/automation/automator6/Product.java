0ac2e14b6457ceb9f97bb66b01ef8656fda9aed5
hdls19 <41329018+hdls19@users.noreply.github.com>
2020-03-22 14:19:24 +0200

    Added product parse from string

--- src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Product.java
+++ src/main/java/ua/com/qatestlab/prestashop/automation/automator6/Product.java
@@ -1,11 +1,52 @@
 package ua.com.qatestlab.prestashop.automation.automator6;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Product {
 
+	private static final Logger LOGGER = LogManager.getLogger(Product.class);
+	
 	private int discount;
-	private float oldPrice;
+	private float oldPrice = -1;
 	private float price;
 	private String currency;
+	
+	public static Product parseProduct(String s) throws Exception {
+		int discount = 0;
+		try {
+			discount = Integer.parseInt(StringParser.parseString(s, "%", "discount-percentage", ">", "-"));
+		}
+		catch (Exception e) {
+			//LOGGER.warn("Failed to parse discount");
+		}
+		
+		float oldPrice = -1;
+		try {
+			String oldPriceText = StringParser.parseString(s, "<", "regular-price", ">");
+			String[] split = oldPriceText.split("&nbsp;");
+			oldPrice = Float.parseFloat(split[0].replace(",", "."));
+		}
+		catch (Exception e) {
+			//LOGGER.warn("Failed to parse old price");
+		}
+		
+		String priceText = StringParser.parseString(s, "<", "price", "price", ">");
+		String[] split = priceText.split("&nbsp;");
+		float price = Float.parseFloat(split[0].replace(",", "."));
+		String currency = split[1];
+		
+		Product product = new Product();
+		product.setDiscount(discount);
+		product.setCurrency(currency);
+		product.setOldPrice(oldPrice);
+		product.setPrice(price);
+		return product;
+	}
+	
+	public boolean hasDiscount() {
+		return discount > 0;
+	}
 	
 	public int getDiscount() {
 		return discount;
